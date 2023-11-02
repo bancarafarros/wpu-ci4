6fb92bd9477ad81fd77b9f36440e92db94385d4b
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-11-02 12:16:40 +0700

    finish 5 views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/', 'Home::coba');
+// $routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::coba');
 // $routes->get('/Coba', 'Coba::index');
 $routes->get('/Coba/index', 'Coba::index');
 $routes->get('/Coba', 'Coba::about');
@@ -22,3 +22,7 @@ $routes->get('/Coba/(:num)', 'Coba::produk/$1'); // mengambil input dari url
 $routes->get('/Users', 'admin\Users::index'); // akses controller di dalam folder
 $routes->get('/Users/about/(:any)/(:any)', 'admin\Users::about/$1/$2'); // akses controller di dalam folder
 $routes->get('/Users/produk/(:any)', 'admin\Users::produk/$1'); // akses controller di dalam folder
+
+$routes->get('/', 'Pages::index');
+$routes->get('/Pages', 'Pages::index');
+$routes->get('/Pages/about', 'Pages::about');
---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab labore sapiente non, autem aperiam hic saepe harum assumenda repellat! Dolorum voluptatum ex repellendus nam architecto nemo perspiciatis eos aliquid itaque!</p>
+        </div>
+    </div>
+</div>
