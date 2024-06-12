e4d00c4b8099673eb82745183c0050ddf48ec123
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-12 13:36:19 +0700

    add dynamic title via Pages Controller

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,14 +6,22 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'Home'
+        ];
+
+        echo view('layout/header', $data);
         return view('pages/home');
         echo view('layout/footer');
     }
 
     public function about()
     {
-        echo view('layout/header');
+        $data = [
+            'title' => 'About'
+        ];
+
+        echo view('layout/header', $data);
         return view('pages/about');
         echo view('layout/footer');
     }
