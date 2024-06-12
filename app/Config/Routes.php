d2ee997717c95bd9ef02ca964be0e468d4f06fa7
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-12 13:36:39 +0700

    add routes Pages Controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-// ci akan membuat jalu yang metode requestnya get dengan url / akan diarahkan ke controlle Home function index
-$routes->get('/', 'Home::index');
-$routes->get('/', 'Home::coba');
+// biar bisa akses controller langsung
+$routes->setAutoRoute(true);
+
+// ci akan membuat jalur yang metode requestnya get dengan url / akan diarahkan ke controlle Home function index
+// $routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::coba');
 
 // akses controller tanpa method
 // $routes->get('/Coba/yeyey', function () {
@@ -21,13 +24,8 @@ $routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2'); // mengambil segment u
 
 $routes->get('/users', 'Admin\Users::index'); // akses url /users dan arahkan ke folder Admin controller Users dan method index
 
-
-
-
-
-
-
-
-
-// biar bisa akses controller langsung
-$routes->setAutoRoute(true);
+// routes untuk controller Pages
+$routes->get('/', 'Pages::index'); // routes dengan request method GET yang mengakses url / dan diarahkan ke Controller Pages function index
+$routes->get('pages/home', 'Pages::index');
+$routes->get('pages/about', 'Pages::about');
+$routes->get('pages/contact', 'Pages::contact');
