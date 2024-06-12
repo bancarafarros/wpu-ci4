3182b7bad3b11d35dfa51c8156710aa1dc81def6
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-12 13:33:39 +0700

    add dive di view

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,2 +1,8 @@
-<h1>About Me</h1>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, tempora. Maiores nam sunt nemo reprehenderit debitis iste illo officiis, sit impedit. Voluptate, numquam magnam culpa assumenda voluptatem vel sunt debitis!</p>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, tempora. Maiores nam sunt nemo reprehenderit debitis iste illo officiis, sit impedit. Voluptate, numquam magnam culpa assumenda voluptatem vel sunt debitis!</p>
+        </div>
+    </div>
+</div>
