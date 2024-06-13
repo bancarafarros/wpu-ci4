a7b47b6bf81079762bb47ad9b6e029070dddfb7b
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-13 22:49:43 +0700

    menggunakan partials, memisahkan komponen navbar

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -9,21 +9,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Gonn</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    <a class="nav-link" href="/pages/about">About</a>
-                    <a class="nav-link" href="/pages/contact">Contact</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?= $this->include('layout/navbar'); ?> <!-- menggunakan partial, panggil (komponen) navbar -->
     <!-- end of header -->
 
     <!-- untuk memanggil 'content' yang merupakan isi halaman yang berasal dari file lain-->
