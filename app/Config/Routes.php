f7453f65eb5e96787aca14ec170b9026fcc66a72
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-06-13 22:52:52 +0700

    finish 6 View Layout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,9 +17,9 @@ $routes->setAutoRoute(true);
 // $routes->get('/Coba/yeyey', function () {
 //     echo "Hello World!";
 // });
-$routes->get('/coba', 'Coba::index'); // akses url /coba dan arahkan ke controller Coba method index
-$routes->get('/coba/index', 'Coba::index'); // akses url /coba/index dan arahkan ke controller Coba method index
-$routes->get('/coba/about', 'Coba::about'); // akses url /coba/about dan arahkan ke controller Coba method about
+$routes->get('/coba', 'Coba::index'); // routes dengan request method GET yang mengakses url /coba dan diarahkan ke Controller Coba function index
+$routes->get('/coba/index', 'Coba::index'); // routes dengan request method GET yang mengakses url /coba/index dan diarahkan ke Controller Coba function index
+$routes->get('/coba/about', 'Coba::about'); // routes dengan request method GET yang mengakses url /coba/about dan diarahkan ke Controller Coba function about
 $routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2'); // mengambil segment url menjadi value
 
 $routes->get('/users', 'Admin\Users::index'); // akses url /users dan arahkan ke folder Admin controller Users dan method index
